0bda98c188d35ddc3afdceeacf4723286c083c01
Giov4 <giovbran03@gmail.com>
2020-09-15 00:52:12 +0200

    Added the possibility to hide wielded items in-game

--- wieldview/init.lua
+++ wieldview/init.lua
@@ -54,9 +54,13 @@ wieldview.update_wielded_item = function(self, player)
 		return
 	end
 	if self.wielded_item[name] then
+		if player:get_meta():get_int("show_wielded_item") == 2 then
+			item = ""
+		end
 		if self.wielded_item[name] == item then
 			return
 		end
+		
 		armor.textures[name].wielditem = self:get_item_texture(item)
 		armor:update_player_visuals(player)
 	end
